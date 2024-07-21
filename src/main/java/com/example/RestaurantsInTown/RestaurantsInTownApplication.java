2f15dbfbf4b0404b1b461c0aee2d92007d20dd02
Hristo Kazanliev <118056676+HristoKazanliev@users.noreply.github.com>
2024-07-21 18:54:40 +0300

    Initial commit

---
+++ src/main/java/com/example/RestaurantsInTown/RestaurantsInTownApplication.java
@@ -0,0 +1,13 @@
+package com.example.RestaurantsInTown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantsInTownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantsInTownApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RestaurantsInTown/RestaurantsInTownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RestaurantsInTown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantsInTownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
